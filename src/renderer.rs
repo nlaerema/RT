e1c6af15d0b6cd11f308be42e5fd6e0785e5b11a
nlaerema <laeremans.nils@gmail.com>
2025-11-08 22:25:55 +0100

    create instance and surface

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,12 +4,21 @@ use winit::window::Window;
 
 pub struct Renderer {
 	window: Arc<Window>,
+	surface: wgpu::Surface<'static>,
 }
 
 impl Renderer {
 	pub async fn new(window: Window) -> anyhow::Result<Self> {
+
+		let window = Arc::new(window);
+
+		let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::from_env_or_default());
+
+		let surface = instance.create_surface(window.clone())?;
+
 		Ok(Self {
-			window: Arc::new(window),
+			window,
+			surface,
 		})
 	}
 
